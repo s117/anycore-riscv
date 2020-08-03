cc474bd38d76e39c259ccd5af34aab95596378f7
Jiayang Li <admin@0x10c.pw>
2020-08-02 20:06:07 -0400

    update build script

--- build-all.sh
+++ build-all.sh
@@ -10,11 +10,11 @@
 #source scl_source enable devtoolset-3
 
 #Path to the directory where the compiled RISC-V tools should be installed
-RISCV_INSTALL=$PWD/install
+export RISCV_INSTALL=$PWD/install
 
 ### Set the correct compiler path if using a custom GCC build
-#GCC_PATH=/ece/ericro/common/gcc492_64
-#GCC_SUFFIX=492
+#GCC_PATH=/ece/ericro/common/gcc920_64
+#GCC_SUFFIX=920
 
 #export CC=$GCC_PATH/bin/gcc$GCC_SUFFIX
 #export CXX=$GCC_PATH/bin/g++$GCC_SUFFIX
@@ -41,11 +41,9 @@ fi
 echo "Starting RISC-V Toolchain build process"
 
 build_project riscv-fesvr --prefix=$RISCV_INSTALL
-build_project riscv-isa-sim --prefix=$RISCV_INSTALL --with-fesvr=$RISCV_INSTALL 
-#build_project riscv-isa-sim --prefix=$RISCV_INSTALL --with-fesvr=$RISCV_INSTALL --enable-simpoint
-CC=riscv64-unknown-elf-gcc CFLAGS="-msoft-float" ASFLAGS="-msoft-float" build_project riscv-pk --prefix=$RISCV_INSTALL/riscv64-unknown-elf --host=riscv --disable-atomics
-build_project riscv-dpi --prefix=$RISCV_INSTALL --with-fesvr=$RISCV_INSTALL --enable-checker 
-#build_project riscv-dpi --prefix=$RISCV_INSTALL --with-fesvr=$RISCV_INSTALL --enable-checker --enable-micro-debug
-
+build_project riscv-isa-sim --prefix=$RISCV_INSTALL --with-fesvr=$RISCV_INSTALL --enable-simpoint
+CFLAGS="-D__riscv64 -march=rv64imfd -mabi=lp64d"  ASFLAGS="-march=rv64imfd -mabi=lp64d" build_project riscv-pk --prefix=$RISCV_INSTALL/riscv64-unknown-elf --host=riscv64-unknown-elf --disable-atomics
+build_project riscv-dpi --prefix=$RISCV_INSTALL --with-fesvr=$RISCV_INSTALL --enable-checker  #--enable-micro-debug
 
 echo -e "\\nRISC-V Toolchain installation completed!"
+
--- build-gcc.sh
+++ build-gcc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script to build RISC-V ISA simulator, proxy kernel, and RISC-V DPI code required for RTL simulations.
+# Script to build RISC-V GNU Toolchain.
 
 # This file uses source code from the University of Berkeley RISC-V project
 # in original or modified form.
@@ -13,8 +13,8 @@
 export RISCV_INSTALL=$PWD/install
 
 ### Set the correct compiler path if using a custom GCC build
-#GCC_PATH=/afs/eos.ncsu.edu/lockers/research/ece/ericro/common/gcc492_64
-#GCC_SUFFIX=492
+#GCC_PATH=/afs/eos.ncsu.edu/lockers/research/ece/ericro/common/gcc920_64
+#GCC_SUFFIX=920
 
 #export CC=$GCC_PATH/bin/gcc$GCC_SUFFIX
 #export CXX=$GCC_PATH/bin/g++$GCC_SUFFIX
@@ -32,11 +32,4 @@ export RISCV_INSTALL=$PWD/install
 
 source ./build.common
 
-#if [ ! `which riscv64-unknown-elf-gcc` ]
-echo "Starting RISC-V GNU Toolchain build process"
-
-build_project riscv-gnu-toolchain --prefix=$RISCV_INSTALL
-
-
-echo -e "RISC-V GNU Toolchain installation completed!"
-
+build_gcc_newlib riscv-gnu-toolchain --prefix=$RISCV_INSTALL --with-arch=rv64imfd --with-abi=lp64d
